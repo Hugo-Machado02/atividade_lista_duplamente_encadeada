e9712df1f34086f315ba0a35544c7b4499a0e8db
Hugo Machado <92072405+Hugo-Machado02@users.noreply.github.com>
2023-12-07 03:44:28 -0300

    Update

--- src/main/java/com/mycompany/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
+++ src/main/java/com/mycompany/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
@@ -12,14 +12,32 @@ public class ListaDuplamenteEncadeada {
 
     public static void main(String[] args) {
         FuncLista fl = new FuncLista();
-        int numeros[] = new int[20];
+        int numeros[] = new int[1000];
         int i, num_min = -9999, num_max = 9999;
+        int num_maior = -9999, num_menor = 9999;
         
-        for(i = 0; i< 20; i++){
+        for(i = 0; i< 1000; i++){
             numeros[i] = (int)(Math.random() * (num_max - num_min + 1) + num_min);
         }
-        for(i = 0; i< 20; i++){
-            System.out.println("Número["+i+"]: "+numeros[i]);
+        for(i = 0; i< 1000; i++){
+            System.out.println("Números: "+numeros[i]);
         }
+        for(i = 0; i< 1000; i++){
+            if(num_maior <= numeros[i]){
+                fl.addFinal(numeros[i]);
+                num_maior = numeros[i];
+            }
+            
+            else if(num_menor >= numeros[i]){
+                fl.addInicio(numeros[i]);
+                num_menor = numeros[i];
+            }
+            
+            else{
+                fl.addMeio(fl.buscaPosicao(numeros[i]), numeros[i]);
+            }
+        }
+        fl.imprimirLista();
+        System.out.println("============================= VALIDAR PRIMOS =============================");
     }
 }
